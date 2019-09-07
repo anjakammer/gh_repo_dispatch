0c6b73f1764b8c4b927d5f09b4502ce2b410793e
Anja Kammer <post@anjakammer.de>
2019-09-07 15:08:29 +0200

    implementation

---
+++ README.md
@@ -0,0 +1,11 @@
+### Triggers a GitHub webhook
+
+## Required Environment Variables
+
+| Key             | Description                                               |
+| ----------------|-----------------------------------------------------------|
+| `APP_NAME`      | developer or App Name                                     |
+| `REPO`          | repository name                                           |
+| `OWNER`         | repository owner                                          |
+| `EVENT_TYPE`    | webhook event name                                        |
+| `TOKEN`         | jwt token to do api calls                                 |
---
+++ index.js
@@ -0,0 +1,34 @@
+const requestp = require('request-promise')
+
+const repoDispatch = (url, body, token) => requestp({
+  url,
+  json: true,
+  headers: {
+    'Authorization': 'token ' + token,
+    'User-Agent': process.env.APP_NAME,
+    'Accept': 'application/vnd.github.everest-preview+json'
+  },
+  method: 'POST',
+  body: {
+    body
+  }
+})
+
+const repo = process.env.REPO
+const owner = process.env.OWNER
+const apiURL = `http://api.github.com/repos/${owner}/${repo}/dispatches`
+const body = `{ event_type: ${process.env.EVENT_TYPE} }`
+
+if (typeof process.env.TOKEN === 'undefined') {
+  console.error('No token defined! Abort.')
+} else {
+  repoDispatch(apiURL, body, process.env.TOKEN)
+    .then((response) => {
+      console.log('Response: ' + response)
+      return response
+    }).catch((error) => {
+      console.error(error)
+    })
+}
+
+module.exports = { repoDispatch }
